4ee4feb7f903c0a690fff20ff9e7ca2561d0e606
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-17 23:26:44 +0300

    Added Common components

---
+++ CC.Common/BaseWorker.cs
@@ -0,0 +1,23 @@
+﻿using CC.Common.Models;
+using Microsoft.Extensions.Logging;
+
+namespace CC.Common
+{
+    public class BaseWorker
+    {
+        protected readonly ILogger Logger;
+
+        public BaseWorker(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        protected void WriteBaseResponse(BaseResponse? response, Exception e, string text)
+        {
+            Logger.LogError(e, text);
+            if (response == null)
+                return;
+            response.ErrorMessage = e.Message;
+        }
+    }
+}
---
+++ CC.Common/Models/BaseResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CC.Common.Models
+{
+    public class BaseResponse
+    {
+        public bool HasError => !string.IsNullOrEmpty(ErrorMessage);
+        public string? ErrorMessage { get; set; }
+    }
+}
