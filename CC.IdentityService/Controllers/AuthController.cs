c6e3df0edbfc76f7165f8af10c35ca0bcb62ee9f
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-18 15:32:34 +0300

    Added additional swagger descriptions to apis

--- CC.IdentityService/Controllers/AuthController.cs
+++ CC.IdentityService/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 ﻿using CC.Common;
+using CC.Common.Models;
 using CC.IdentityService.Interfaces;
 using CC.IdentityService.Models.Requests;
+using CC.IdentityService.Models.Responses;
 using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -26,14 +28,18 @@ namespace CC.IdentityService.Controllers
 
         //Should not be access without authentication 
         [HttpGet("test")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(401, Type = typeof(void))]
         public IActionResult Test()
         {
             return Ok("Authenticated successfully.");
         }
-        
+
         //TODO:This API is for generating users, available scopes: notify.scope, identity.scope and upload.scope
         [AllowAnonymous]
         [HttpPost("register")]
+        [ProducesResponseType(204, Type = typeof(void))]
+        [ProducesResponseType(400, Type = typeof(BaseResponse))]
         public async Task<IActionResult> Register(RegisterRequest request)
         {
             var validationResult = await _registerValidator.ValidateAsync(request);
@@ -46,6 +52,8 @@ namespace CC.IdentityService.Controllers
 
         [AllowAnonymous]
         [HttpPost("login")]
+        [ProducesResponseType(200, Type = typeof(AuthResponse))]
+        [ProducesResponseType(400, Type = typeof(AuthResponse))]
         public async Task<IActionResult> Login(AuthRequest request)
         {
             var validationResult = await _authValidator.ValidateAsync(request);
