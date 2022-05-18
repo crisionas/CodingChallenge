f150e9bf87581e8f685080ff945dea57175e7f69
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-18 15:10:36 +0300

    Added auth controller with Authentication and test method and Validation injection

---
+++ CC.IdentityService/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using CC.Common;
+using CC.IdentityService.Interfaces;
+using CC.IdentityService.Models.Requests;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC.IdentityService.Controllers
+{
+    [Authorize]
+    [Route("auth")]
+    [ApiController]
+    public class AuthController :ControllerBase
+    {
+        private readonly IValidator<RegisterRequest> _registerValidator;
+
+        private readonly IAuthWorker _authWorker;
+
+        public AuthController(IAuthWorker authWorker, IValidator<RegisterRequest> registerValidator)
+        {
+            _authWorker = authWorker;
+            _registerValidator = registerValidator;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var validationResult=await _registerValidator.ValidateAsync(request);
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult);
+            var response = await _authWorker.RegistrationAsync(request);
+            return Ok(response);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(AuthRequest request)
+        {
+            var response = await _authWorker.AuthenticateAsync(request);
+            return Ok(response);
+        }
+
+        [HttpPost("test")]
+        public IActionResult Test(AuthRequest request)
+        {
+            return Ok("test");
+        }
+    }
+}
