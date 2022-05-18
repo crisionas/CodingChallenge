4ada7d847901cf1c7ad2fe49734fea048079f396
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-18 22:13:35 +0300

    Added common userRequestSettings handler

---
+++ CC.Common/IUserRequestSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace CC.Common
+{
+    public interface IUserRequestSettings
+    {
+        public string? Username { get; }
+        public string? Company { get; }
+        public string? Email { get; }
+    }
+}
---
+++ CC.Common/UserRequestSettings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace CC.Common
+{
+    public class UserRequestSettings : IUserRequestSettings
+    {
+        private readonly IList<Claim>? _claims;
+        public UserRequestSettings(IHttpContextAccessor accessor)
+        {
+            _claims = accessor.HttpContext?.User.Claims.ToList();
+        }
+
+        public string? Username => _claims?.FirstOrDefault(x => x.Type == ClaimTypes.Name)?.Value;
+        public string? Company => _claims?.FirstOrDefault(x => x.Type == "company")?.Value;
+        public string? Email => _claims?.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+    }
+}
