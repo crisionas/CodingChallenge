6ccb550e54d5b5832c327868164c7264d8799a9c
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-18 15:10:08 +0300

    added first fluent validation

---
+++ CC.IdentityService/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,41 @@
+﻿using CC.IdentityService.Models.Requests;
+using CC.IdentityService.Models.Settings;
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
+namespace CC.IdentityService.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator(IOptions<IdentitySettings> options)
+        {
+            var settings = options.Value;
+
+            ClassLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .WithErrorCode(StatusCodes.Status400BadRequest.ToString());
+
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithErrorCode(StatusCodes.Status400BadRequest.ToString());
+
+            RuleFor(x => x.Company)
+                .NotEmpty()
+                .WithErrorCode(StatusCodes.Status400BadRequest.ToString());
+
+            RuleFor(x => x.Scopes)
+                .NotNull()
+                .WithErrorCode(StatusCodes.Status400BadRequest.ToString())
+                .ForEach(x =>
+                {
+                    x.NotEmpty()
+                        .WithErrorCode(StatusCodes.Status400BadRequest.ToString())
+                        .Must(g => settings.AuthCredentials.Select(a => a.Scope).Contains(g))
+                        .WithErrorCode(StatusCodes.Status400BadRequest.ToString())
+                        .WithMessage("The scopes are invalid.");
+                });
+        }
+    }
+}
