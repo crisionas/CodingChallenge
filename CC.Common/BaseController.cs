16a522b5c5ebc50bb6bcef6c486a89672d726e93
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-18 15:15:41 +0300

    Added a generic baseResponse for returning responses

---
+++ CC.Common/BaseController.cs
@@ -0,0 +1,63 @@
+﻿using CC.Common.Models;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CC.Common
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        #region protected methods
+
+        /// <summary>
+        /// Prepare IActionResult with Ok Response
+        /// </summary>
+        /// <param name="response">Object</param>
+        /// <returns>IActionResult</returns>
+        protected IActionResult PrepareActionResult(object? response)
+        {
+            return response == null ? NoContent() : response is BaseResponse baseResponse ? GetResponse(baseResponse) : Ok(response);
+        }
+
+        /// <summary>
+        /// Prepare IActionResult with NoContent response
+        /// </summary>
+        /// <param name="response">Object</param>
+        /// <returns>IActionResult</returns>
+        protected IActionResult PrepareNoContentResult(BaseResponse response) => response.HasError ? GetResponse(response) : NoContent();
+
+        /// <summary>
+        /// Prepare IActionResult from ValidationResult
+        /// </summary>
+        /// <param name="validationResult">ValidationResult</param>
+        /// <returns>IActionResult</returns>
+        protected IActionResult GetResponseFromValidationResult(ValidationResult validationResult)
+        {
+            var errorCode = validationResult.Errors.FirstOrDefault()?.ErrorCode;
+            var response = new BaseResponse
+            {
+                ErrorMessage = validationResult.Errors.Select(e => e.ErrorMessage).Aggregate((total, current) => $"{total} {current}"),
+            };
+            return GetResponse(response);
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Return a IActionResult based if baseResponse has error or not
+        /// </summary>
+        /// <param name="response">BaseResponse</param>
+        /// <returns>IActionResult</returns>
+        private IActionResult GetResponse(BaseResponse response)
+        {
+            if (response.HasError)
+                return BadRequest(response);
+            return Ok(response);
+        }
+
+        #endregion
+
+    }
+}
