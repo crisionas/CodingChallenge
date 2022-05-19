c1d9a6de578ebf0a3aaea614882bdc151663c2a7
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-19 20:08:06 +0300

    Move Emailmessage as Common

---
+++ CC.Common/Models/EmailMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace CC.Common.Models
+{
+    public class EmailMessage
+    {
+        /// <summary>
+        /// Email where to send
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Subject of the notification
+        /// </summary>
+        public string? Subject { get; set; }
+
+        /// <summary>
+        /// Message that will be send within notification
+        /// </summary>
+        public string? Message { get; set; }
+
+    }
+}
