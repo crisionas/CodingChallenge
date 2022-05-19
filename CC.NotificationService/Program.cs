a4748069db82a2ebe0d9de633138933b418c99ff
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-19 14:44:25 +0300

    Added Common project, SwaggerGenOptions and BindService class

--- CC.NotificationService/Program.cs
+++ CC.NotificationService/Program.cs
@@ -1,11 +1,15 @@
+using CC.Common;
+using CC.NotificationService.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.BindServices(builder.Configuration);
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGenOptions();
 
 var app = builder.Build();
 
