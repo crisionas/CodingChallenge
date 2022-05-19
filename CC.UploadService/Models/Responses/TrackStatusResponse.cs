bba730d1e3d992ec29211314fcbcabf888754cac
Cristian Ionaș <cris.ionas@gmail.com>
2022-05-19 14:32:54 +0300

    Added TrackStatusResponse Model (Upload)

---
+++ CC.UploadService/Models/Responses/TrackStatusResponse.cs
@@ -0,0 +1,9 @@
+﻿using CC.Common.Models;
+
+namespace CC.UploadService.Models.Responses
+{
+    public class TrackStatusResponse : BaseResponse
+    {
+        public string? Status { get; set; }
+    }
+}
